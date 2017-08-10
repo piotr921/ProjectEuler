998253056211bdc65ad6e757102acb903aa16626
Piotr Sekula <piotr92345@o2.pl>
2017-08-10 17:51:57 +0200

    Count sum of 3 and 5 multipliers

--- src/main/java/pl/javaacademy/euler/threeandfivemultipliers/Main.java
+++ src/main/java/pl/javaacademy/euler/threeandfivemultipliers/Main.java
@@ -1,8 +1,21 @@
 package pl.javaacademy.euler.threeandfivemultipliers;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        long sum = IntStream.range(0, 1000)
+                .filter(number -> (isMultiplyOfFive(number) || isMultiplyOfThree(number)))
+                .sum();
+        System.out.println("Sum is: " + sum);
+    }
+
+    private static boolean isMultiplyOfFive(int number) {
+        return number % 5 == 0;
+    }
+
+    private static boolean isMultiplyOfThree(int number) {
+        return number % 3 == 0;
     }
 }
